c52e54bb45c896f322175a33dde04a1edf2ebac2
ayeshaenayat <ayeshaenayat123enayat@gmail.com>
2024-09-01 19:12:52 +0500

    add reset function

--- 15_chapter 49 to 52/app.js
+++ 15_chapter 49 to 52/app.js
@@ -1,21 +1,24 @@
-    //    question1
+       //question1
 
-// document.getElementById('signupForm').addEventListener('submit', function(event) {
-//     event.preventDefault(); // Prevent form submission
+document.getElementById('signupForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
 
-    // Get form data
-//     const name = document.getElementById('name').value;
-//     const email = document.getElementById('email').value;
-//     const password = document.getElementById('password').value;
+    //Get form data
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
 
-//     // Display form data
-//     document.getElementById('displayName').textContent = name;
-//     document.getElementById('displayEmail').textContent = email;
-//     document.getElementById('displayPassword').textContent = password;
+    // Display form data
+    document.getElementById('displayName').textContent = name;
+    document.getElementById('displayEmail').textContent = email;
+    document.getElementById('displayPassword').textContent = password;
 
-//     // Show the display data section
-//     document.getElementById('displayData').classList.remove('hidden');
-// });
+    // Show the display data section
+    document.getElementById('displayData').classList.remove('hidden');
+
+    document.getElementById("signupForm").reset();
+
+});
 
 
 //   -------           question2 --------
